e65ef09d37c3ce20144bcec08cc6cc4ec95fdf88
deroude <rvgabriel@yahoo.com>
2021-06-27 19:26:28 +0300

    moved index

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/delegate/TemplateConfig';
-export * from './src/delegate/TemplateDelegate';
-export * from './src/util/TemplateUtils';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './delegate/TemplateConfig';
+export * from './delegate/TemplateDelegate';
+export * from './util/TemplateUtils';
